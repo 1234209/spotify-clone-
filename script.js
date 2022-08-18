a586a08f3061158782553293ada5103cf67f0f1f
bhagya nanditha <67181954+1234209@users.noreply.github.com>
2022-08-18 21:47:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Random bgm 1 ", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Random bgm 2", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Random bgm 3", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Random bgm 4", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Random bgm  5 filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Random bgm 6, filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Random bgm 7, filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Random bgm 8, filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Random bgm 9, filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Random bgm01, filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
